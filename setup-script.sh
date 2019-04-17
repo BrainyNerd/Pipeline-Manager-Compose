07346819f021dd3541603ba0340e0e16fe7d860c
Chandrakant <in-chandrakant.kaski@in-chandrakatkaski.local>
2019-04-17 12:33:10 +0530

    [Chandrakant][#] automating project setup

---
+++ setup-script.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -e
+
+git clone https://github.com/BrainyNerd/pipeline-manager-api.git
+cd Pipeline-Manager
+git clone https://github.com/BrainyNerd/pipeline-manager-web.git
+
